527bf0fff0bea741ba9656476765ea190a7bd51f
ilyas <ilyashepelev23@gmail.com>
2026-01-26 17:26:44 +0300

    chore: move folders into app dir

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <AppHeader/>
+      <slot/>
+<!--    <UContainer class="fixed bottom-0 w-full grid sm:hidden place-items-center my-3">-->
+<!--      <AddBookmarkModal />-->
+<!--    </UContainer>-->
+  </div>
+</template>
+<script setup lang="ts">
+// import AddBookmarkModal from "~/layers/bookmark/components/AddBookmarkModal.vue";
+import AppHeader from "~/layers/base/components/global/AppHeader.vue";
+</script>
