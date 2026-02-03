a94cb9757abcd1eb85aefa469afc84bb4b8dc3bc
ilyas <ilyashepelev23@gmail.com>
2026-02-03 13:11:43 +0300

    feat(ui): add Save Link modal

---
+++ layers/bookmark/app/components/AddBookmarkModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <UModal
+      title="Save Link"
+      description="Paste a link and save it for later"
+      :close="{
+        color: 'neutral',
+    }">
+    <UButton
+        label="Save link"
+        color="primary"
+    />
+
+    <template #body>
+      <div class="grid gap-1">
+        <LinkPreview
+            v-if="preview"
+            :preview="preview"/>
+        <DynamicForm
+            :state="state"
+            :schema="schema"
+            class="grid gap-4 h-fit"
+            :validation-schema="bookmarkSchema"/>
+      </div>
+    </template>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+import {bookmarkSchema} from "~~/layers/bookmark/validators/bookmarkRules"
+import DynamicForm from "~~/layers/form/app/components/global/DynamicForm.vue";
+import LinkPreview from "~~/layers/bookmark/app/components/LinkPreview.vue";
+import {useLinkPreview} from "~~/layers/bookmark/app/composables/useLinkPreview"
+import {shallowReactive} from "vue"
+
+const schema = computed(() => getFormSchema("bookmark", "bookmark"))
+
+const state = shallowReactive({
+  link: ""
+})
+
+const {preview, fetchPreview} = useLinkPreview()
+
+watch(() => state.link, async () => {
+  await fetchPreview(state.link)
+})
+</script>
+
+<style scoped>
+
+</style>
