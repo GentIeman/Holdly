6f079096d46257d71d5990b395679112d23bae1a
ilyas <ilyashepelev23@gmail.com>
2026-02-03 13:09:30 +0300

    refactor(api): fetch preview via API endpoint

--- layers/bookmark/app/composables/useLinkPreview.ts
+++ layers/bookmark/app/composables/useLinkPreview.ts
@@ -1,48 +1,39 @@
-import { shallowRef } from 'vue'
+import {shallowRef} from 'vue'
 import type {LinkPreview} from "~~/layers/bookmark/app/components/LinkPreview.vue"
 
 export function useLinkPreview() {
     const preview = shallowRef<LinkPreview | null>(null)
     const error = shallowRef<Error | null>(null)
-
-    const getMetaContent = (doc: Document, selector: string) => {
-        const element = doc.querySelector(selector) as HTMLMetaElement | null
-        return element?.content || null
-    }
+    const toast = useToast()
 
     const fetchPreview = async (url: string) => {
-        error.value = null
-
-        if (!url) return
-
-        if (url.length < 1) {
-            resetPreview()
-            return
-        }
-
-        try {
-            const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
-            const data = await response.json()
-            const html = data.contents
-
-            const parser = new DOMParser()
-            const doc = parser.parseFromString(html, 'text/html')
-
-            preview.value = {
-                title: getMetaContent(doc, 'meta[property="og:title"]') || doc.title,
-                description: getMetaContent(doc, 'meta[property="og:description"]') ||
-                    getMetaContent(doc, 'meta[name="description"]'),
-                image: getMetaContent(doc, 'meta[property="og:image"]'),
-                siteName: getMetaContent(doc, 'meta[property="og:site_name"]')
-            }
-        } catch (err) {
-            error.value = err as Error
-        }
-    }
-
-    const resetPreview = () => {
         preview.value = null
         error.value = null
+
+        if (url.length < 5) return
+
+        preview.value = await $fetch('/api/link-preview', {
+            query: {url},
+            onResponseError({response}) {
+                if (response.status == 400) {
+                    toast.add({
+                        title: 'Invalid link',
+                        description: 'Please check that the URL is correct and publicly accessible.',
+                        color: 'error',
+                    })
+                }
+
+                if (response.status == 504) {
+                    toast.add({
+                        title: 'Request timed out',
+                        description: 'Please try again later or use a different link.',
+                        color: 'error',
+                    })
+                }
+
+                toast.clear()
+            }
+        })
     }
 
     return {
