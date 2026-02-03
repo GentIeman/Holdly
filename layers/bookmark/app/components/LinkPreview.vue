337f5608250e66929ff81b94b62589359d55ab08
ilyas <ilyashepelev23@gmail.com>
2026-02-03 13:07:41 +0300

    refactor(ui): update link preview template structure

--- layers/bookmark/app/components/LinkPreview.vue
+++ layers/bookmark/app/components/LinkPreview.vue
@@ -1,13 +1,17 @@
 <template>
-  <section>
-    <p class="text-primary font-bold">{{ preview?.siteName }}</p>
-    <h1 class="font-bold">{{ preview?.title }}</h1>
-    <h2 class="line-clamp-2 text-neutral-300">{{ preview?.description }}</h2>
-    <img
-        v-if="preview?.image"
-        :src="preview?.image"
-        alt="Link preview">
-  </section>
+  <article
+      v-if="preview"
+      class="grid gap-3">
+    <div>
+      <p class="text-primary font-bold">{{ preview.siteName }}</p>
+      <h3 class="font-bold">{{ preview.title }}</h3>
+    </div>
+      <img
+          v-if="preview.image"
+          :src="preview.image"
+          alt="Link preview"
+      >
+  </article>
 </template>
 
 <script setup lang="ts">
