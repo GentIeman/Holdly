a08e9c82ab737cf68fade7b0fc4c33d9cda52c27
ilyas <ilyashepelev23@gmail.com>
2026-02-03 14:49:16 +0300

    chore: uncomment Save Link modal

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <AppHeader/>
-      <slot/>
-<!--    <UContainer class="fixed bottom-0 w-full grid sm:hidden place-items-center my-3">-->
-<!--      <AddBookmarkModal />-->
-<!--    </UContainer>-->
+    <slot/>
+    <UContainer
+        class="fixed bottom-0 w-full grid sm:hidden place-items-center py-5 bg-linear-to-b to-neutral-950">
+      <AddBookmarkModal/>
+    </UContainer>
   </div>
 </template>
 <script setup lang="ts">
-// import AddBookmarkModal from "~/layers/bookmark/components/AddBookmarkModal.vue";
+import AddBookmarkModal from "~~/layers/bookmark/app/components/AddBookmarkModal.vue";
 import AppHeader from "~~/layers/base/app/components/global/AppHeader.vue";
 </script>
