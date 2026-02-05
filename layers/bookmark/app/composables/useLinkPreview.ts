55b19b3fdf65fc75a52ab424a7bbb06359d1c665
ilyas <ilyashepelev23@gmail.com>
2026-02-05 15:32:16 +0300

    refactor: rename preview to metadata

--- layers/bookmark/app/composables/useLinkPreview.ts
+++ layers/bookmark/app/composables/useLinkPreview.ts
@@ -1,18 +1,18 @@
 import {shallowRef} from 'vue'
-import type {LinkPreview} from "~~/layers/bookmark/app/components/LinkPreview.vue"
+import type {LinkMetaData} from "~~/layers/bookmark/app/components/LinkPreview.vue"
 
-export function useLinkPreview() {
-    const preview = shallowRef<LinkPreview | null>(null)
+export function useLinkMetaData() {
+    const metadata = shallowRef<LinkMetaData | null>(null)
     const error = shallowRef<Error | null>(null)
     const toast = useToast()
 
-    const fetchPreview = async (url: string) => {
-        preview.value = null
+    const fetchMetaData = async (url: string) => {
+        metadata.value = null
         error.value = null
 
         if (url.length < 5) return
 
-        preview.value = await $fetch('/api/link-metadata', {
+        metadata.value = await $fetch('/api/link-metadata', {
             query: {url},
             onResponseError({response}) {
                 if (response.status == 400) {
@@ -37,8 +37,8 @@ export function useLinkPreview() {
     }
 
     return {
-        preview,
+        metadata,
         error,
-        fetchPreview,
+        fetchMetaData,
     }
 }
