2583bae201282a95bda8458a57eec3688bc953ac
ilyas <ilyashepelev23@gmail.com>
2026-02-18 14:57:56 +0300

    refactor: use resettable state for modal form

--- layers/bookmark/app/components/AddBookmarkModal.vue
+++ layers/bookmark/app/components/AddBookmarkModal.vue
@@ -1,10 +1,12 @@
 <template>
   <UModal
+      v-model:open="isModalOpen"
       title="Save Link"
       description="Paste a link and save it for later"
       :close="{
         color: 'neutral',
-    }">
+      }"
+      @after:leave="resetState">
     <UButton
         label="Save link"
         color="primary"
@@ -34,21 +36,20 @@ import DynamicForm from "~~/layers/form/app/components/global/DynamicForm.vue";
 import LinkPreview from "~~/layers/bookmark/app/components/LinkPreview.vue";
 import {useLinkPreview} from "~~/layers/bookmark/app/composables/useLinkPreview"
 import {shallowReactive} from "vue"
+import {useResettableFormState} from "~~/layers/form/app/composables/useResettableFormState"
 
 const schema = computed(() => getFormSchema("bookmark", "bookmark"))
 
-const state = shallowReactive({
-  link: ""
 type BookmarkState = {
   link: string
-  user: User["documentId"]
 }
-const state = shallowReactive<BookmarkState>({
-  link: "",
-  user: user.value.documentId
-})
 
 const {preview, fetchPreview} = useLinkPreview()
+const {state, resetState} = useResettableFormState<BookmarkState>(() => ({
+  link: ""
+}))
+
+const isModalOpen = ref(false)
 
 watch(() => state.link, async () => {
   await fetchPreview(state.link)
