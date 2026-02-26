8276024d580ee27256e1ee287175ebf9b63ae8e1
ilyas <ilyashepelev23@gmail.com>
2026-02-26 15:00:47 +0300

    refactor(useAuth): remove login and register functions

--- layers/user/app/composables/useAuth.ts
+++ layers/user/app/composables/useAuth.ts
@@ -1,42 +1,8 @@
-import type {Response as LoginResponse} from "~~/layers/user/server/api/login.post"
-import type {Response as RegisterResponse} from "~~/layers/user/server/api/register.post"
 import type {Response as MeResponse} from "~~/layers/user/server/api/me.get"
 
 export const useAuth = () => {
     const user = useUser();
     const toast = useToast()
-
-    const login = async (identifier: string, password: string) => {
-        const response = await $fetch<LoginResponse>("/api/login", {
-            method: "POST",
-            body: {identifier, password},
-            onResponseError({ response } ) {
-                toast.add({
-                    title: "Trouble signing in?",
-                    description: response._data.data.error.message,
-                    color: "error"
-                })
-            }
-        });
-
-        user.value = response.user
-    };
-
-    const register = async (email: string, password: string, username: string) => {
-        const response = await $fetch<RegisterResponse>("/api/register", {
-            method: "POST",
-            body: {email, password, username},
-            async onResponseError({ response } ) {
-                toast.add({
-                    title: " Oops, something went wrong!",
-                    description: response._data.data.error.message,
-                    color: "error"
-                })
-            }
-        });
-
-        user.value = response.user
-    };
 
     const me = async () => {
         const requestFetch = useRequestFetch()
@@ -52,5 +18,5 @@ export const useAuth = () => {
         user.value = response.user
     };
 
-    return {login, register, me};
+    return {me}
 };
