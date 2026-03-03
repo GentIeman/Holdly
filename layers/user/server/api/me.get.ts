76fdf6fc07cbcc1cf59406429246fd7b7ac10205
ilyas <ilyashepelev23@gmail.com>
2026-03-03 14:18:13 +0300

    refactor(api): return user directly

--- layers/user/server/api/me.get.ts
+++ layers/user/server/api/me.get.ts
@@ -1,4 +1,3 @@
-import {getCookie} from "h3";
 import type {User} from "~~/layers/user/app/composables/useUser"
 
 export type Response = {
@@ -10,13 +9,11 @@ export default defineEventHandler(async (event) => {
 
     const token = getCookie(event, "strapi_jwt")
 
-    const user =  await $fetch<Response>(config.public.strapiOrigin + "/api/users/me", {
+    return await $fetch<Response>(config.public.strapiOrigin + "/api/users/me", {
         method: "GET",
         headers: {
             "Content-type": "application/json",
             Authorization: `Bearer ${token}`,
-        },
+        }
     })
-
-    return {user}
 })
