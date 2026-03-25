ca6cab6d754cc3b7d4749d462b85f425164feec2
ilyas <ilyashepelev23@gmail.com>
2026-03-25 14:19:46 +0300

    refactor(signInForm): add typed responses

--- layers/user/app/components/signInForm.vue
+++ layers/user/app/components/signInForm.vue
@@ -59,8 +59,12 @@ const toast = useToast()
 const router = useRouter()
 const user = useUser()
 
+type LoginResponse = {
+  user: User
+}
+
 async function handleSubmit(event: FormSubmitEvent<Schema>) {
-  user.value = await $fetch("/api/login", {
+  user.value = await $fetch<LoginResponse>("/api/login", {
     method: "POST",
     body: {
       identifier: event.data.email,
