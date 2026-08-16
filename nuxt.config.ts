77a3ecb43ddba37f054179c8c84a30adc8cf1701
ilyas <ilyashepelev23@gmail.com>
2026-08-16 17:14:20 +0300

    chore: drop the @nuxt/ui module and dependency since no components use it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: false },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt"],
+  modules: ["@nuxt/eslint", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
   app: {
     head: {
